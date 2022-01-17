3df5e450e0cdea1a3b59e7111b4e1291685602a4
ReyDonovan <gennady.zamalaev@gmail.com>
2022-01-17 12:04:22 +0300

    Core/OpenSSL: some update

--- src/server/shared/Cryptography/HMACSHA1.cpp
+++ src/server/shared/Cryptography/HMACSHA1.cpp
@@ -21,54 +21,52 @@
 #include "BigNumber.h"
 #include "Common.h"
 
-
 #if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L
 HMAC_CTX* HMAC_CTX_new()
 {
-	HMAC_CTX *ctx = new HMAC_CTX();
-	HMAC_CTX_init(ctx);
-	return ctx;
+    HMAC_CTX *ctx = new HMAC_CTX();
+    HMAC_CTX_init(ctx);
+    return ctx;
 }
 
 void HMAC_CTX_free(HMAC_CTX* ctx)
 {
-	HMAC_CTX_cleanup(ctx);
-	delete ctx;
+    HMAC_CTX_cleanup(ctx);
+    delete ctx;
 }
-
 #endif
-HmacHash::HmacHash(uint32 len, uint8 *seed)
+
+HmacHash::HmacHash(uint32 len, uint8 *seed) : _ctx(HMAC_CTX_new())
 {
-	m_ctx = HMAC_CTX_new();
-	HMAC_Init_ex(m_ctx, seed, len, EVP_sha1(), nullptr);
+	HMAC_Init_ex(_ctx, seed, len, EVP_sha1(), nullptr);
 	memset(m_digest, 0, sizeof(m_digest));
 }
 
 HmacHash::~HmacHash()
 {
-	HMAC_CTX_free(m_ctx);
+	HMAC_CTX_free(_ctx);
 }
 
-void HmacHash::UpdateData(const std::string &str)
+void HmacHash::UpdateData(std::string const& str)
 {
-    HMAC_Update(m_ctx, (uint8 const*)str.c_str(), str.length());
+    HMAC_Update(_ctx, reinterpret_cast<uint8 const*>(str.c_str()), str.length());
 }
 
-void HmacHash::UpdateData(const uint8* data, size_t len)
+void HmacHash::UpdateData(uint8 const* data, int len)
 {
-    HMAC_Update(m_ctx, data, len);
+    HMAC_Update(_ctx, data, len);
 }
 
 void HmacHash::Finalize()
 {
     uint32 length = 0;
-    HMAC_Final(m_ctx, (uint8*)m_digest, &length);
+    HMAC_Final(_ctx, (uint8*)m_digest, &length);
     ASSERT(length == SHA_DIGEST_LENGTH);
 }
 
 uint8 *HmacHash::ComputeHash(BigNumber* bn)
 {
-    HMAC_Update(m_ctx, bn->AsByteArray().get(), bn->GetNumBytes());
+    HMAC_Update(_ctx, bn->AsByteArray().get(), bn->GetNumBytes());
     Finalize();
     return (uint8*)m_digest;
 }
